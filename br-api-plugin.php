e89eaff347e21e150337a167c1496a9687826bc5
rajandangi <rajandangi649@gmail.com>
2022-03-13 08:17:02 +0545

    A Plugin for Wordpress User Registration and Sign API

---
+++ br-api-plugin.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ *
+ * Plugin Name: WP Vue API BR
+ * Plugin URI: https://www.wordpress.org
+ * Description: A WP API for vue.
+ * Version: 1.0.0
+ * Author: Bam Kadayat
+ * Author URI: https://www.wordpress.org
+ * License: GPL v3
+ * Text-Domain: textdomain
+ */
+
+if (!defined('ABSPATH')) exit(); // No direct access allowed
+
+
+/**
+ * Require Autoloader
+ */
+require_once 'vendor/autoload.php';
+
+use WPVABR\Api\Api;
+
+// use WPVBR\Includes\Admin;
+// use WPVBR\Includes\Frontend;
+// use WPVBR\Includes\PageTemplater;
+
+
+
+final class WP_Vue_API_BR
+{
+
+    /**
+     * Define Plugin Version
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Construct Function
+     */
+    public function __construct()
+    {
+        $this->plugin_constants();
+        register_activation_hook(__FILE__, [$this, 'activate']);
+        register_deactivation_hook(__FILE__, [$this, 'deactivate']);
+        add_action('plugins_loaded', [$this, 'init_plugin']);
+    }
+
+    /**
+     * Plugin Constants
+     * @since 1.0.0
+     */
+    public function plugin_constants()
+    {
+        define('WPVABR_VERSION', self::VERSION);
+        define('WPVABR_PLUGIN_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+        define('WPVABR_PLUGIN_URL', trailingslashit(plugins_url('', __FILE__)));
+        define('WPVABR_NONCE', 'b?l#3&45@_*(+3&[G[xAc8O~Fv)*36*7^_$piN0.N%N~X91VbCn@.41233');
+    }
+
+    /**
+     * Singletone Instance
+     * @since 1.0.0
+     */
+    public static function init()
+    {
+        static $instance = false;
+
+        if (!$instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * On Plugin Activation
+     * @since 1.0.0
+     */
+    public function activate()
+    {
+        $is_installed = get_option('wpvabr_is_installed');
+
+        if (!$is_installed) {
+            update_option('wpvabr_is_installed', time());
+        }
+
+        update_option('wpvabr_is_installed', WPVABR_VERSION);
+    }
+
+    /**
+     * On Plugin De-activation
+     * @since 1.0.0
+     */
+    public function deactivate()
+    {
+        // On plugin deactivation
+    }
+
+    /**
+     * Init Plugin
+     * @since 1.0.0
+     */
+    public function init_plugin()
+    {
+        // init
+        // new Admin();
+        //new Frontend();
+        new Api();
+        // new PageTemplater();
+    }
+}
+
+/**
+ * Initialize Main Plugin
+ * @since 1.0.0
+ */
+function wp_vue_api_br()
+{
+    return WP_Vue_API_BR::init();
+}
+
+// Run the Plugin
+wp_vue_api_br();
